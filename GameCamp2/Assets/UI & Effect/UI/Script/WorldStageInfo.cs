81ff377e8f2f4b4e3f5d367f980d5a031f9ae57a
Makuri <kidc24588@gmail.com>
2018-11-09 21:43:23 +0900

    Worldmap씬에 UI 추가
    Worldmap 씬에 있는 UI 추가
    Ready 버튼을 누르면 라운드가 실행되도록 추가할 예정

---
+++ GameCamp2/Assets/UI & Effect/UI/Script/WorldStageInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStageInfo : MonoBehaviour
+{
+    public bool OnActive = false;
+    public float speed = 1.0f;
+    [SerializeField] Vector2 hidePosition = new Vector2();
+    [SerializeField] Vector2 ActivePosition = new Vector2();
+    public string stageName = null;
+    [SerializeField] UILabel stageNameLabel = null; 
+
+    // Use this for initialization
+    void Start()
+    {
+        ActivePosition = Vector2.zero;
+        hidePosition = new Vector2(950.0f, 0.0f);
+        transform.localPosition = hidePosition;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateActive();
+    }
+
+    void UpdateActive()
+    {
+        if (OnActive)
+        {
+            transform.localPosition = Vector2.Lerp(transform.localPosition, ActivePosition, Time.deltaTime * speed);
+            stageNameLabel.text = stageName;
+
+        }
+        else
+        {
+            transform.localPosition = Vector2.Lerp(transform.localPosition, hidePosition, Time.deltaTime * speed);
+        }
+    }
+}
